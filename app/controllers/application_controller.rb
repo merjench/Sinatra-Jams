e949592f9e7e3c8ec96832403bd8ca2a972e4d18
merjench <merjen.chary@gmail.com>
2019-04-03 16:35:16 -0400

    Met CR deliverables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,23 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    @artist = Artist.all
+    erb :index
   end
 
+  get "/show/:id" do
+    @artist = Artist.find(params[:id])
+    erb :show
+  end
+
+  get "/artists/new" do
+    erb :new
+  end
+
+  post '/artist' do
+    @artist = params[:artist]
+    @new_artist = Artist.create(@artist)
+    redirect '/show/' + @new_artist.id.to_s
+  #  redirect '/artist/#{new_artist}'
+  end
 end
